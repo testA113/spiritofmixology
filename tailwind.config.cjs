441039f1c9e78a8c0c7a8639d088e8ee939942eb
testA113 <83188384+testA113@users.noreply.github.com>
2024-02-11 21:53:59 +1300

    jiggly button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,18 +17,32 @@ module.exports = {
         heading: ['Concert One', ...defaultTheme.fontFamily.sans],
       },
       animation: {
-        fadein: 'fadein 1s forwards',
+        jiggle1: 'jiggle1 0.33s infinite',
+        jiggle2: 'jiggle2 0.4s infinite',
       },
       keyframes: {
-        fadein: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+        jiggle1: {
+          '0%, 100%': { transform: 'translate(0, 0) rotate(0deg)' },
+          '25%': { transform: 'translate(5px, 0) rotate(-4deg)' },
+          '50%': { transform: 'translate(0, -5px) rotate(3deg)' },
+          '75%': { transform: 'translate(-5px, 0) rotate(6deg)' },
+        },
+        jiggle2: {
+          '0%, 100%': { transform: 'translate(0, 3) rotate(0deg)' },
+          '25%': { transform: 'translate(-5px, 3px) rotate(3deg)' },
+          '50%': { transform: 'translate(2px, -5px) rotate(-2deg)' },
+          '75%': { transform: 'translate(5px, -2px) rotate(4deg)' },
         },
       },
       backgroundImage: {
         noise:
           "url('https://webflow-files-prod.global.ssl.fastly.net/647d7a596d3b30dafe7f7d07/647d7b5e1383c7b582a4ff02_grain-tile_500x500.webp')",
       },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['hover'],
     },
   },
   plugins: [require('@tailwindcss/typography')],
