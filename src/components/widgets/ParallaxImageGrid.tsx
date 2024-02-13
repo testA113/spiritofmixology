114c6ad0031bc89f64942a2295d094eb8412983a
testA113 <83188384+testA113@users.noreply.github.com>
2024-02-13 23:53:27 +1300

    less parallax

--- src/components/widgets/ParallaxImageGrid.tsx
+++ src/components/widgets/ParallaxImageGrid.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const cursorScalingFactor = 0.05;
 const scrollScalingFactor = 0.1;
 
 import festivalCocktailPreparing from '/src/assets/images/festival-cocktail-preparing.jpg';
 import cocktailVendor from '/src/assets/images/cocktail-vendor.jpg';
-import aperolGinSour from '/src/assets/images/aperol-gin-sour.jpg';
+import erricks from '/src/assets/images/erricks.jpg';
 
 export function ParallaxImageGrid() {
   const [cursor, setCursor] = useState({ x: 0, y: 0 });
@@ -32,20 +32,57 @@ export function ParallaxImageGrid() {
       window.removeEventListener('scroll', handleWindowScroll);
     };
   }, []);
+  const isMobile = window.innerWidth <= 768;
 
   return (
     <ImageGrid
-      translateX={-cursor.x * cursorScalingFactor + scrollPosition.x * scrollScalingFactor}
-      translateY={-cursor.y * cursorScalingFactor + scrollPosition.y * scrollScalingFactor}
+      translateX={isMobile ? scrollPosition.x * scrollScalingFactor : -cursor.x * cursorScalingFactor}
+      translateY={isMobile ? scrollPosition.y * scrollScalingFactor : -cursor.y * cursorScalingFactor}
     />
   );
 }
 
 function ImageGrid({ translateX, translateY }: { translateX: number; translateY: number }) {
+  const imagesRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const images = imagesRef?.current?.getElementsByTagName('img');
+    if (!images) {
+      return;
+    }
+
+    const addBlur = () => {
+      for (let img of images) {
+        img.classList.add('blur-md', 'md:blur-lg');
+        img.classList.remove('hover:blur-0');
+      }
+    };
+
+    const removeBlur = () => {
+      for (let img of images) {
+        img.classList.remove('blur-md', 'md:blur-lg');
+        img.classList.add('hover:blur-0');
+      }
+    };
+
+    for (let img of images) {
+      img.addEventListener('mouseover', removeBlur);
+      img.addEventListener('mouseout', addBlur);
+    }
+
+    return () => {
+      for (let img of images) {
+        img.removeEventListener('mouseover', removeBlur);
+        img.removeEventListener('mouseout', addBlur);
+      }
+    };
+  }, []);
+
   return (
     <div className="h-full w-full flex flex-col justify-center items-center relative opacity-100 overflow-hidden">
       <div
-        className="grid w-full h-full grid-6 md:grid-cols-10 lg:grid-cols-12"
+        ref={imagesRef}
+        className="grid w-full h-full grid-cols-3 md:grid-cols-10 lg:grid-cols-12"
         style={{
           transform: `translate3d(${translateX}px, ${translateY}px, 0)`,
         }}
@@ -53,17 +90,17 @@ function ImageGrid({ translateX, translateY }: { translateX: number; translateY:
         <img
           src={festivalCocktailPreparing.src}
           alt="at a festival, a bartender skillfully pours a vodka from a height to prepare a cocktail"
-          className="col-start-4 col-end-6 row-start-1 row-end-2 blur-md md:blur-lg hover:blur-0 transition-all duration-500"
+          className="col-start-4 col-end-6 row-start-1 row-end-2 blur-md md:blur-lg transition-all md:hover:scale-105 duration-500 -rotate-1"
         ></img>
         <img
           src={cocktailVendor.src}
           alt="a cocktail stall serving earger customers"
-          className="col-start-9 col-end-12 row-start-2 row-end-3 blur-md md:blur-lg hover:blur-0 transition-all duration-500"
+          className="col-start-9 col-end-12 row-start-2 row-end-3 blur-md md:blur-lg transition-all md:hover:scale-105 duration-500 rotate-3"
         ></img>
         <img
-          src={aperolGinSour.src}
-          alt="aperol gin sour cocktail"
-          className="col-start-3 col-end-5 row-start-3 row-end-4 blur-md md:blur-lg hover:blur-0 transition-all duration-500"
+          src={erricks.src}
+          alt="wide angle of erricks venue with a band playing"
+          className="col-start-3 col-end-7 lg:col-end-6 xl:col-end-5 row-start-3 row-end-4 blur-md md:blur-lg transition-all md:hover:scale-105 duration-500 -rotate-2"
         ></img>
       </div>
     </div>
