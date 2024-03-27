34ea1f2ab953ef18c63fd220944f83b30046830b
testa113 <testa113>
2024-03-28 08:27:51 +1300

    instagram and tickets

--- src/navigation.js
+++ src/navigation.js
@@ -13,13 +13,14 @@ export const headerData = {
       href: '#contact',
     },
   ],
-  actions: [{ text: 'get your tickets', href: 'https://google.com' }],
+  actions: [{ text: 'get your tickets', href: 'https://erricks.flicket.co.nz/events/9795434d-d0ac-4297-8c8b-ba696207165d' }],
 };
 
 export const footerData = {
   links: [],
   secondaryLinks: [],
   socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/spiritofmixologyfest' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/spiritofmixologyfest' },
   ],
   footNote: 'Made with ♥️ by Ali Harris',
